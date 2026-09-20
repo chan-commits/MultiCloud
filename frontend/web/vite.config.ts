3edaf22e155ef4683b9193f16462c00d0637608c
chan <34831410+chan-commits@users.noreply.github.com>
2026-09-20 17:27:51 +0000

    Upgrade SvelteKit to 3 prerelease

--- frontend/web/vite.config.ts
+++ frontend/web/vite.config.ts
@@ -1,9 +1,25 @@
 import tailwindcss from '@tailwindcss/vite';
+import adapter from '@sveltejs/adapter-static';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [tailwindcss(), sveltekit()],
+  plugins: [
+    tailwindcss(),
+    sveltekit({
+      alias: {
+        '$lib': 'src/lib',
+      },
+      adapter: adapter({
+        pages: 'dist',
+        assets: 'dist',
+        fallback: 'index.html',
+      }),
+      compilerOptions: {
+        runes: true,
+      },
+    }),
+  ],
   server: {
     port: 5173,
     proxy: {
